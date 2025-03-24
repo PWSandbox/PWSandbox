0348d80887da27a3027f437417f74a8e924c322c
yarb00 <yaroslav.biba@outlook.com>
2025-03-24 21:52:51 +0800

    redesign menu (launcher) & add 'about app' dialog (#5)
    - Apply new EditorConfig to each code file
    - Redesign menu (launcher)
    - Implement light/dark theme switch in menu (launcher)
    - Add 'About PWSandbox' dialog with app version, description, copyright and license
    - Add a copy of the MIT License text to each code file

--- src/AboutForm.cs
+++ src/AboutForm.cs
@@ -26,70 +26,43 @@
 
 namespace PWSandbox;
 
-public enum Theme
+public partial class AboutForm : Form
 {
-	Light,
-	Dark
-}
-
-public partial class MenuForm : Form
-{
-	private Theme currentColorTheme = Theme.Dark;
-
-	public MenuForm()
+	public AboutForm()
 	{
 		InitializeComponent();
 
 		appVersionLabel.Text = $"v{System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(3)}";
-	}
 
-	private void LoadMapFile(object sender, EventArgs e)
-	{
-		if (mapFileLocationTextBox.Text != "")
-		{
-			PlayForm playForm = new PlayForm(mapFileLocationTextBox.Text);
-			if (!playForm.IsDisposed) playForm.Show();
-		}
-		else
-		{
-			MessageBox.Show(
-				"ERROR loading PWSandbox:"
-				+ "\nmap file location is NOT specified!",
-				"PWSandbox",
-				MessageBoxButtons.OK,
-				MessageBoxIcon.Error,
-				MessageBoxDefaultButton.Button1
-			);
-			return;
-		}
-	}
+		appDescriptionTextBox.Text =
+@"Simple sandbox game, built with .NET and Windows Forms.
 
-	private void SwitchTheme(object sender, EventArgs e)
-	{
-		Theme newTheme = currentColorTheme == Theme.Light ? Theme.Dark : Theme.Light;
+You can find PWSandbox Git repository at https://github.com/yarb00/PWSandbox
 
-		Color backColor = Color.Transparent, foreColor = Color.Transparent;
+This software is licensed under the MIT (X11) License. You can find its text below.
 
-		if (newTheme == Theme.Light)
-		{
-			backColor = Color.White;
-			foreColor = Color.Black;
-		}
-		else if (newTheme == Theme.Dark)
-		{
-			backColor = Color.Black;
-			foreColor = Color.White;
-		}
+=====
 
-		currentColorTheme = newTheme;
+MIT License
 
-		BackColor = backColor;
-		ForeColor = foreColor;
-	}
+Copyright (c) 2024-2025 yarb00
 
-	private void OpenAboutAppDialog(object sender, EventArgs e)
-	{
-		AboutForm aboutForm = new();
-		aboutForm.ShowDialog();
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the ""Software""), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.";
 	}
 }
--- src/MenuForm.Designer.cs
+++ src/MenuForm.Designer.cs
@@ -1,125 +1,135 @@
 // This file is a part of PWSandbox ( https://github.com/yarb00/PWSandbox )
+// PWSandbox is licensed under the MIT (X11) License:
+
+/* MIT License
+ *
+ * Copyright (c) 2024 - 2025 yarb00
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace PWSandbox;
 
 partial class MenuForm
 {
-    private System.ComponentModel.IContainer components = null;
+	private System.ComponentModel.IContainer components = null;
 
-    protected override void Dispose(bool disposing)
-    {
-        if (disposing && (components != null))
-        {
-            components.Dispose();
-        }
-        base.Dispose(disposing);
-    }
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing && (components != null)) components.Dispose();
+		base.Dispose(disposing);
+	}
 
-    #region Windows Form Designer generated code
+	#region Windows Form Designer generated code
 
-    private void InitializeComponent()
-    {
-        appNameLabel = new Label();
-        githubLinkLabel = new Label();
-        appVersionLabel = new Label();
-        authorLabel = new Label();
-        mapFileLocationTextBox = new TextBox();
-        loadMapFileButton = new Button();
-        SuspendLayout();
-        // 
-        // appNameLabel
-        // 
-        appNameLabel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-        appNameLabel.AutoSize = true;
-        appNameLabel.Font = new Font("Consolas", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        appNameLabel.Location = new Point(194, 9);
-        appNameLabel.Name = "appNameLabel";
-        appNameLabel.Size = new Size(258, 56);
-        appNameLabel.TabIndex = 0;
-        appNameLabel.Text = "PWSandbox";
-        appNameLabel.TextAlign = ContentAlignment.TopRight;
-        // 
-        // githubLinkLabel
-        // 
-        githubLinkLabel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-        githubLinkLabel.AutoSize = true;
-        githubLinkLabel.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        githubLinkLabel.Location = new Point(256, 65);
-        githubLinkLabel.Name = "githubLinkLabel";
-        githubLinkLabel.Size = new Size(196, 14);
-        githubLinkLabel.TabIndex = 1;
-        githubLinkLabel.Text = "github.com/yarb00/pwsandbox";
-        githubLinkLabel.TextAlign = ContentAlignment.TopRight;
-        // 
-        // appVersionLabel
-        // 
-        appVersionLabel.AutoSize = true;
-        appVersionLabel.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        appVersionLabel.Location = new Point(12, 9);
-        appVersionLabel.Name = "appVersionLabel";
-        appVersionLabel.Size = new Size(49, 14);
-        appVersionLabel.TabIndex = 2;
-        appVersionLabel.Text = "v1.2.0";
-        // 
-        // authorLabel
-        // 
-        authorLabel.AutoSize = true;
-        authorLabel.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        authorLabel.Location = new Point(12, 23);
-        authorLabel.Name = "authorLabel";
-        authorLabel.Size = new Size(70, 14);
-        authorLabel.TabIndex = 3;
-        authorLabel.Text = "by yarb00";
-        // 
-        // mapFileLocationTextBox
-        // 
-        mapFileLocationTextBox.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-        mapFileLocationTextBox.Location = new Point(12, 287);
-        mapFileLocationTextBox.Name = "mapFileLocationTextBox";
-        mapFileLocationTextBox.PlaceholderText = "Path to map (*.pwsandbox) file";
-        mapFileLocationTextBox.Size = new Size(354, 22);
-        mapFileLocationTextBox.TabIndex = 4;
-        mapFileLocationTextBox.Text = "map.pwsandbox";
-        // 
-        // loadMapFileButton
-        // 
-        loadMapFileButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-        loadMapFileButton.Location = new Point(372, 288);
-        loadMapFileButton.Name = "loadMapFileButton";
-        loadMapFileButton.Size = new Size(80, 21);
-        loadMapFileButton.TabIndex = 5;
-        loadMapFileButton.Text = "Load map!";
-        loadMapFileButton.UseVisualStyleBackColor = true;
-        loadMapFileButton.Click += LoadMap;
-        // 
-        // MenuForm
-        // 
-        AutoScaleMode = AutoScaleMode.None;
-        ClientSize = new Size(464, 321);
-        Controls.Add(loadMapFileButton);
-        Controls.Add(mapFileLocationTextBox);
-        Controls.Add(authorLabel);
-        Controls.Add(appVersionLabel);
-        Controls.Add(githubLinkLabel);
-        Controls.Add(appNameLabel);
-        Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        FormBorderStyle = FormBorderStyle.FixedSingle;
-        MaximizeBox = false;
-        MaximumSize = new Size(480, 360);
-        MinimumSize = new Size(480, 360);
-        Name = "MenuForm";
-        ShowIcon = false;
-        Text = "PWSandbox";
-        ResumeLayout(false);
-        PerformLayout();
-    }
+	private void InitializeComponent()
+	{
+		appNameLabel = new Label();
+		mapFileLocationTextBox = new TextBox();
+		loadMapFileButton = new Button();
+		aboutAppButton = new Button();
+		appVersionLabel = new Label();
+		SuspendLayout();
+		//
+		// appNameLabel
+		//
+		appNameLabel.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+		appNameLabel.Font = new Font("Consolas", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+		appNameLabel.Location = new Point(12, 9);
+		appNameLabel.Name = "appNameLabel";
+		appNameLabel.Size = new Size(440, 56);
+		appNameLabel.TabIndex = 0;
+		appNameLabel.Text = "PWSandbox";
+		appNameLabel.TextAlign = ContentAlignment.MiddleCenter;
+		appNameLabel.Click += SwitchTheme;
+		//
+		// mapFileLocationTextBox
+		//
+		mapFileLocationTextBox.Location = new Point(12, 68);
+		mapFileLocationTextBox.Name = "mapFileLocationTextBox";
+		mapFileLocationTextBox.Size = new Size(354, 22);
+		mapFileLocationTextBox.TabIndex = 4;
+		mapFileLocationTextBox.Text = "map.pws_map";
+		//
+		// loadMapFileButton
+		//
+		loadMapFileButton.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+		loadMapFileButton.FlatStyle = FlatStyle.Flat;
+		loadMapFileButton.Font = new Font("Consolas", 7F);
+		loadMapFileButton.Location = new Point(372, 69);
+		loadMapFileButton.Name = "loadMapFileButton";
+		loadMapFileButton.Size = new Size(80, 21);
+		loadMapFileButton.TabIndex = 5;
+		loadMapFileButton.Text = "Load map!";
+		loadMapFileButton.UseVisualStyleBackColor = false;
+		loadMapFileButton.Click += LoadMapFile;
+		//
+		// aboutAppButton
+		//
+		aboutAppButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+		aboutAppButton.FlatStyle = FlatStyle.Flat;
+		aboutAppButton.Location = new Point(12, 106);
+		aboutAppButton.Name = "aboutAppButton";
+		aboutAppButton.Size = new Size(440, 23);
+		aboutAppButton.TabIndex = 7;
+		aboutAppButton.Text = "About PWSandbox";
+		aboutAppButton.UseVisualStyleBackColor = false;
+		aboutAppButton.Click += OpenAboutAppDialog;
+		//
+		// appVersionLabel
+		//
+		appVersionLabel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+		appVersionLabel.Location = new Point(372, 9);
+		appVersionLabel.Name = "appVersionLabel";
+		appVersionLabel.Size = new Size(80, 14);
+		appVersionLabel.TabIndex = 10;
+		appVersionLabel.Text = "Version ?";
+		appVersionLabel.TextAlign = ContentAlignment.TopRight;
+		//
+		// MenuForm
+		//
+		AutoScaleMode = AutoScaleMode.None;
+		BackColor = Color.Black;
+		ClientSize = new Size(464, 141);
+		Controls.Add(appVersionLabel);
+		Controls.Add(aboutAppButton);
+		Controls.Add(loadMapFileButton);
+		Controls.Add(mapFileLocationTextBox);
+		Controls.Add(appNameLabel);
+		Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+		ForeColor = Color.White;
+		FormBorderStyle = FormBorderStyle.FixedSingle;
+		MaximizeBox = false;
+		MaximumSize = new Size(480, 180);
+		MinimumSize = new Size(480, 180);
+		Name = "MenuForm";
+		ShowIcon = false;
+		Text = "PWSandbox";
+		ResumeLayout(false);
+		PerformLayout();
+	}
 
-    #endregion
+	#endregion
 
-    private Label appNameLabel;
-    private Label githubLinkLabel;
-    private Label appVersionLabel;
-    private Label authorLabel;
-    private TextBox mapFileLocationTextBox;
-    private Button loadMapFileButton;
+	private Label appNameLabel;
+	private TextBox mapFileLocationTextBox;
+	private Button loadMapFileButton;
+	private Button aboutAppButton;
+	private Label appVersionLabel;
 }
