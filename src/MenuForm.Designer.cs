b9e7319a4d6db69a7f092ded10db95ec33136455
yarb00 <yaroslav.biba@outlook.com>
2025-04-23 06:59:30 +0800

    main menu redesign & OpenFileDialog support
    - Redesign main menu (launcher)
    - Add "Check for updates" button
    - Move theme switching into a separate button
    - Replace manual path typing (TextBox) with OpenFileDialog

--- src/MenuForm.Designer.cs
+++ src/MenuForm.Designer.cs
@@ -24,9 +24,6 @@
  * SOFTWARE.
  */
 
-using System.Drawing;
-using System.Windows.Forms;
-
 namespace PWSandbox;
 
 partial class MenuForm
@@ -43,53 +40,33 @@ partial class MenuForm
 
 	private void InitializeComponent()
 	{
-		appNameLabel = new Label();
-		mapFileLocationTextBox = new TextBox();
-		loadMapFileButton = new Button();
-		aboutAppButton = new Button();
-		appVersionLabel = new Label();
+		appNameLabel = new System.Windows.Forms.Label();
+		aboutAppButton = new System.Windows.Forms.Button();
+		appVersionLabel = new System.Windows.Forms.Label();
+		switchColorThemeButton = new System.Windows.Forms.Button();
+		checkForUpdatesButton = new System.Windows.Forms.Button();
+		mapOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+		loadMapFileButton = new System.Windows.Forms.Button();
 		SuspendLayout();
 		//
 		// appNameLabel
 		//
-		appNameLabel.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-		appNameLabel.Font = new Font("Consolas", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
-		appNameLabel.Location = new Point(12, 9);
+		appNameLabel.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+		appNameLabel.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+		appNameLabel.Location = new System.Drawing.Point(12, 9);
 		appNameLabel.Name = "appNameLabel";
-		appNameLabel.Size = new Size(440, 56);
+		appNameLabel.Size = new System.Drawing.Size(440, 56);
 		appNameLabel.TabIndex = 0;
 		appNameLabel.Text = "PWSandbox";
-		appNameLabel.TextAlign = ContentAlignment.MiddleCenter;
-		appNameLabel.Click += SwitchTheme;
-		//
-		// mapFileLocationTextBox
-		//
-		mapFileLocationTextBox.Location = new Point(12, 68);
-		mapFileLocationTextBox.Name = "mapFileLocationTextBox";
-		mapFileLocationTextBox.Size = new Size(354, 22);
-		mapFileLocationTextBox.TabIndex = 4;
-		mapFileLocationTextBox.Text = "map.pws_map";
-		//
-		// loadMapFileButton
-		//
-		loadMapFileButton.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-		loadMapFileButton.FlatStyle = FlatStyle.Flat;
-		loadMapFileButton.Font = new Font("Consolas", 7F);
-		loadMapFileButton.Location = new Point(372, 69);
-		loadMapFileButton.Name = "loadMapFileButton";
-		loadMapFileButton.Size = new Size(80, 21);
-		loadMapFileButton.TabIndex = 5;
-		loadMapFileButton.Text = "Load map!";
-		loadMapFileButton.UseVisualStyleBackColor = false;
-		loadMapFileButton.Click += LoadMapFile;
+		appNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 		//
 		// aboutAppButton
 		//
-		aboutAppButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-		aboutAppButton.FlatStyle = FlatStyle.Flat;
-		aboutAppButton.Location = new Point(12, 106);
+		aboutAppButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+		aboutAppButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+		aboutAppButton.Location = new System.Drawing.Point(12, 177);
 		aboutAppButton.Name = "aboutAppButton";
-		aboutAppButton.Size = new Size(440, 23);
+		aboutAppButton.Size = new System.Drawing.Size(440, 23);
 		aboutAppButton.TabIndex = 7;
 		aboutAppButton.Text = "About PWSandbox";
 		aboutAppButton.UseVisualStyleBackColor = false;
@@ -97,42 +74,86 @@ partial class MenuForm
 		//
 		// appVersionLabel
 		//
-		appVersionLabel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-		appVersionLabel.Location = new Point(372, 9);
+		appVersionLabel.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+		appVersionLabel.Location = new System.Drawing.Point(340, 9);
 		appVersionLabel.Name = "appVersionLabel";
-		appVersionLabel.Size = new Size(80, 14);
+		appVersionLabel.Size = new System.Drawing.Size(112, 14);
 		appVersionLabel.TabIndex = 10;
 		appVersionLabel.Text = "Version ?";
-		appVersionLabel.TextAlign = ContentAlignment.TopRight;
+		appVersionLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+		//
+		// switchColorThemeButton
+		//
+		switchColorThemeButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+		switchColorThemeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+		switchColorThemeButton.Location = new System.Drawing.Point(12, 119);
+		switchColorThemeButton.Name = "switchColorThemeButton";
+		switchColorThemeButton.Size = new System.Drawing.Size(440, 23);
+		switchColorThemeButton.TabIndex = 11;
+		switchColorThemeButton.Text = "Switch color theme";
+		switchColorThemeButton.UseVisualStyleBackColor = false;
+		switchColorThemeButton.Click += SwitchTheme;
+		//
+		// checkForUpdatesButton
+		//
+		checkForUpdatesButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+		checkForUpdatesButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+		checkForUpdatesButton.Location = new System.Drawing.Point(12, 148);
+		checkForUpdatesButton.Name = "checkForUpdatesButton";
+		checkForUpdatesButton.Size = new System.Drawing.Size(440, 23);
+		checkForUpdatesButton.TabIndex = 12;
+		checkForUpdatesButton.Text = "Check for updates";
+		checkForUpdatesButton.UseVisualStyleBackColor = false;
+		//
+		// mapOpenFileDialog
+		//
+		mapOpenFileDialog.DefaultExt = "pws_map";
+		mapOpenFileDialog.Filter = "PWSandbox map files|*.pws_map";
+		mapOpenFileDialog.Title = "Select a PWSandbox map...";
+		//
+		// loadMapFileButton
+		//
+		loadMapFileButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+		loadMapFileButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+		loadMapFileButton.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+		loadMapFileButton.Location = new System.Drawing.Point(12, 68);
+		loadMapFileButton.Name = "loadMapFileButton";
+		loadMapFileButton.Size = new System.Drawing.Size(440, 46);
+		loadMapFileButton.TabIndex = 13;
+		loadMapFileButton.Text = "Load map!";
+		loadMapFileButton.UseVisualStyleBackColor = false;
+		loadMapFileButton.Click += LoadMapFile;
 		//
 		// MenuForm
 		//
-		AutoScaleMode = AutoScaleMode.None;
-		BackColor = Color.Black;
-		ClientSize = new Size(464, 141);
+		AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+		BackColor = System.Drawing.Color.Black;
+		ClientSize = new System.Drawing.Size(464, 210);
+		Controls.Add(loadMapFileButton);
+		Controls.Add(checkForUpdatesButton);
+		Controls.Add(switchColorThemeButton);
 		Controls.Add(appVersionLabel);
 		Controls.Add(aboutAppButton);
-		Controls.Add(loadMapFileButton);
-		Controls.Add(mapFileLocationTextBox);
 		Controls.Add(appNameLabel);
-		Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-		ForeColor = Color.White;
-		FormBorderStyle = FormBorderStyle.FixedSingle;
+		Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+		ForeColor = System.Drawing.Color.White;
+		FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 		MaximizeBox = false;
-		MaximumSize = new Size(480, 180);
-		MinimumSize = new Size(480, 180);
+		MaximumSize = new System.Drawing.Size(480, 249);
+		MinimumSize = new System.Drawing.Size(480, 249);
 		Name = "MenuForm";
 		ShowIcon = false;
 		Text = "PWSandbox";
 		ResumeLayout(false);
-		PerformLayout();
 	}
 
 	#endregion
 
-	private Label appNameLabel;
-	private TextBox mapFileLocationTextBox;
-	private Button loadMapFileButton;
-	private Button aboutAppButton;
-	private Label appVersionLabel;
+	private System.Windows.Forms.Label appNameLabel;
+	private System.Windows.Forms.Button aboutAppButton;
+	private System.Windows.Forms.Label appVersionLabel;
+	private System.Windows.Forms.Button switchColorThemeButton;
+	private System.Windows.Forms.Button checkForUpdatesButton;
+	private System.Windows.Forms.OpenFileDialog mapOpenFileDialog;
+	private System.Windows.Forms.Button loadMapFileButton;
 }
